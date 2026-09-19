5e24cc442e9b6c87980badac6f20f510826cf80f
Laryssa Souza <laryssa.tech@gmail.com>
2026-09-19 20:02:58 -0300

    Adiciona projeto P14 Páginas de Perguntas Frequentes

---
+++ script.js
@@ -0,0 +1,5 @@
+function alternarResposta(elemento) {
+    const resposta = elemento.querySelector('.resposta');
+    resposta.classList.toggle('mostrar');
+    resposta.classList.toggle('escondido');
+}
